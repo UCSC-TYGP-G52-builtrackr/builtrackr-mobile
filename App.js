14f09c28a84fb9187fb96d2e00d2fbbdef946ede
Rumindu De Silva <rumindukavishka14@gmail.com>
2023-08-05 16:05:16 +0530

    Create login screen as an sketch

--- App.js
+++ App.js
@@ -1,7 +1,50 @@
-import Main from "./Main"
+import { StyleSheet, Text, View, Button, TextInput} from "react-native";
 
-export  default function App(){
-  return(
-    <Main/>
-  )
+export default function App() {
+  return (
+    <View style={styles.appCointainer}>
+      <View style={styles.goalContainer}>
+        <Text style={styles.heading}>Log in to</Text>
+        <Text>BuiltTrackr</Text>
+      </View>
+      <View style={styles.flexView}>
+        <TextInput style={styles.textInput} placeholder="Enter your Email or Username"/> 
+        <TextInput style={styles.textInput} placeholder="Enter your goal!"/> 
+        <Button title="Login"/>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({ 
+  appCointainer:{
+    flex:1, //now take whole available screen size
+    paddingTop:50,
+    paddingHorizontal:16,
+  },
+  flexView:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    // paddingBottom: 24,
+    // borderBottomWidth: 2,
+    // borderBottomColor: '#198a35',
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor:'red',
+    width:'70%',
+    marginRight:17 ,
+    padding: 8,
+  },
+  goalContainer:{
+    flex:4
+  },
+  heading:{
+    fontWeight: 'bold',
+
+  }
+
+  
+});
