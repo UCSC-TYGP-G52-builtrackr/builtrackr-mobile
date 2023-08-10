a80c843bcd7d09bbb0ad531d407feb96c262b1d6
Govindani <govindani2000@gamil.com>
2023-08-10 22:07:08 +0530

    new navigation

--- App.js
+++ App.js
@@ -1,74 +1,12 @@
-import {
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  Image,
-  Pressable,
-  onPress,
-  Button,
-} from 'react-native';
-import Constants from 'expo-constants';
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import Routes from './routes/routing';
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
 
-export default function App() {
-  const [text, onChangeText] = React.useState('');
-  
+
+
+ export default function  App () {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <Image style={styles.logo} source={require('./assets/login.jpg')} />
-
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder="Enter the email"
-      />
-      <TextInput style={styles.input} placeholder="Enter the password" />
-      <Pressable style={styles.button}>
-        <Text>Login</Text>
-      </Pressable>
-      <Text style={styles.texts}> If you don' t Have account</Text>
-    </View>
+<Routes/>
   );
 }
 
-const styles = StyleSheet.create({
-  texts: {
-    padding: 15,
-  },
-  title: {
-    fontWeight: 'bold',
-  },
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 24,
-    marginTop: 50,
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    width: 250,
-  },
-  logo: {
-    height: 350,
-    width: 300,
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: '#ffcc00',
-  },
-});
---
+++ routes/routing.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import { Login } from '../screens/login';
+import { Signup } from '../screens/signup';
+
+const Stack = createNativeStackNavigator();
+
+
+ export default function  Routes() {
+  return (
+<NavigationContainer>
+  <Stack.Navigator>
+    <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="Signup" component={Signup} />
+  </Stack.Navigator>
+</NavigationContainer>
+  );
+}
+
