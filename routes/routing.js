195e822a8a3e1a3d51ed91d81912513cd652fb79
M. R. K. W. DE SILVA <108813657+Rumindu@users.noreply.github.com>
2023-08-30 16:01:54 +0530

    add side drawer

--- routes/routing.js
+++ routes/routing.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from "@react-navigation/native";
+
+
 import { Login } from "../screens/login";
 import { Signup } from "../screens/signup";
 import { SupervisorDashboard } from "../screens/site_supervisor/dashboard";
@@ -8,20 +11,47 @@ import { TaskProof } from "../screens/site_supervisor/taskProof";
 import {CameraComponent} from "../screens/site_supervisor/camera";
 import  Home  from '../screens/Home';
 
+
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="Task Proof"
+        component={TaskProof}
+        options={{
+          title: 'Task Proof',
+        }}
+      />
+      <Drawer.Screen
+        name="Supervisor Dashboard"
+        component={SupervisorDashboard}
+        options={{
+          title: 'Supervisor Dashboard',
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
+
 
 const Routes = () => {
   return(
   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Group>
-        <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
-        <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='Supervisor Dashboard' component={SupervisorDashboard}/>
-        <Stack.Screen name='Task Proof' component={TaskProof} />
-        <Stack.Screen name='Camera' component={CameraComponent} />
-        <Stack.Screen name='Signup' component={Signup} />
-      </Stack.Group>
+    <Stack.Navigator initialRouteName="Drawer">
+      <Stack.Screen options={{headerShown: false}} name="Home" component={Home} />
+    <Stack.Screen
+          name="Drawer"
+          component={DrawerNavigator}
+          options={{headerShown: false}}
+        />
+      <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Supervisor Dashboard' component={SupervisorDashboard}/>
+      <Stack.Screen name='Task Proof' component={TaskProof} />
+      <Stack.Screen name='Camera' component={CameraComponent} />
+      <Stack.Screen name='Signup' component={Signup} />
     </Stack.Navigator>
   </NavigationContainer>
 
