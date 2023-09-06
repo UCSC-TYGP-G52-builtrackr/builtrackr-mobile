d55e559b491ba1571a3bf1dead4b2dbb0fbb5297
Cham-De <chamodilakshani98.com>
2023-09-06 23:58:19 +0530

    login backend done, with authentication and error msgs display

--- screens/login.js
+++ screens/login.js
@@ -1,14 +1,23 @@
-import {Text,View,StyleSheet,TextInput,Image,Pressable,onPress,Button} from 'react-native';
+import {Text, View, StyleSheet, TextInput, Image, Pressable, TouchableOpacity, KeyboardAvoidingView, ScrollView, onPress,Button} from 'react-native';
+
 import { useNavigation } from "@react-navigation/native";
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from "react";
+import { AntDesign } from '@expo/vector-icons';
  
   const Login =({navigation})=> {
     // const [text, onChangeText] = React.useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigation();
+
+    const clearErrorAndInputs = () => {
+      setErrorMessage(''); // Clear the error message
+      setEmail(''); // Reset email input to empty
+      setPassword(''); // Reset password input to empty
+    };
 
     const handleLogin = async () => {
       try {
@@ -36,11 +45,17 @@ import { useState, useEffect } from "react";
           // Check if the login was successful based on the data received from the server
           if (data.success) {
             // Navigate to the "Sites" page
-            navigation.navigate('Sites', { customerID: data.customerID });
+            if (data.userType === 'customer') {
+              // Navigate to the "Sites" page for customers
+              navigation.navigate('Sites', { customerID: data.customerID });
+            } else if (data.userType === 'supervisor') {
+              // Navigate to the "Supervisor Dashboard" for supervisors
+              navigation.navigate("Supervisor Dashboard", { employeeID: data.employeeID });
+            }
           } else {
             // Handle failed login (e.g., show an error message to the user)
-            navigation.navigate("Supervisor Dashboard");
-            // console.error('Login failed:', data.message);
+            setErrorMessage(data.message);
+            // navigation.navigate("Supervisor Dashboard");
           }
         } else {
           // Handle non-successful HTTP responses (e.g., 404 Not Found, 500 Internal Server Error)
@@ -53,10 +68,25 @@ import { useState, useEffect } from "react";
     };
 
     return (
+      <ScrollView>
       <View style={styles.container}>
         <Text style={styles.title}>Login</Text>
         <Image style={styles.logo} source={require('../assets/login.jpg')} />
   
+        {/* Display the error message */}
+        {errorMessage ? (
+          <View style={styles.errorDisplay}>
+            <TextInput
+              value={errorMessage}
+              editable={false}
+              placeholder="Error"
+            />
+            <TouchableOpacity onPress={() => clearErrorAndInputs()}>
+              <AntDesign name="closecircle" size={18} color="red" />
+            </TouchableOpacity>
+          </View>
+        ) : null}
+
         <TextInput
           style={styles.input}
           onChangeText={setEmail}
@@ -78,6 +108,7 @@ import { useState, useEffect } from "react";
           <Text>Login</Text>
         </Pressable>
       </View>
+      </ScrollView>
     );
   }
   
@@ -94,12 +125,24 @@ import { useState, useEffect } from "react";
       alignItems: 'center',
       justifyContent: 'center',
       padding: 24,
-      marginTop: 50,
+      // marginTop: 50,
     },
     input: {
       height: 40,
       margin: 12,
       borderWidth: 1,
+      borderRadius: 10,
+      padding: 10,
+      width: 250,
+    },
+    errorDisplay: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      height: 45,
+      margin: 12,
+      borderWidth: 1,
+      borderColor: 'red',
       borderRadius: 10,
       padding: 10,
       width: 250,
